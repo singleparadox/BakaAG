d553424e9e3f5a03c19afb7cbabbb55ed3998c12
cross_noire <alejandrejames.papina@bicol-u.edu.ph>
2018-01-28 22:58:55 +0800

    admin page link
    +Added a header link for admin users in the page

--- header.php
+++ header.php
@@ -1,3 +1,13 @@
+<?php
+if(session_status() == PHP_SESSION_NONE)
+	session_start();
+
+$error_msg = "";
+if(isset($_SESSION['error_msg'])) {
+	$error_msg = '<label class="show_error" style="position:absolute;">'.$_SESSION['error_msg'].'</label>';
+	unset($_SESSION['error_msg']);
+}
+?>
 <!DOCTYPE html>
 <html>
 <head>
@@ -6,6 +16,11 @@
 </head>
 <body>
 	<header>
+			<?php
+				if(isset($_SESSION['acc_type_id'])) 
+					if(($_SESSION['acc_type_id'])==2)
+						echo '<div style="background-color: green"><center><a href="admin/index.php" class="btn btn-primary"> Go to Admin Panel</a></center></div>';
+			?>
 		<nav class="navbar navbar-expand-lg navbar-dark bg-primary">
 			<a class="navbar-brand" href="index.php">Baka<span id="logo">AG</span></a>
 				<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
@@ -16,16 +31,33 @@
 					<div class="menu-wrap">
 					    <div class="menu">
 					        <ul class="clearfix">
-
-					            <li>
-					                <a href="#"><img id="profile_logo" src="img/profile.png"></a>
-
-					                <ul class="sub-menu">
-					                    <li><a href="#">Profile</a></li>
-					                    <li><a href="#">Settings</a></li>
-					                    <li><a href="#">Logout</a></li>
-					                </ul>
-					            </li>
+					        	<?php
+					        		if(isset($_SESSION['acc_id']))
+					        			echo '
+					        				<li>
+					                			<a href="#"><img id="profile_logo" src="img/profile.png"></a>
+					                				<ul class="sub-menu">
+					                    				<li><a href="#">Profile</a></li>
+					                    				<li><a href="#">Settings</a></li>
+					                    				<li><a href="#">Logout</a></li>
+					                				</ul>
+					            			</li>';
+					            	else
+					            		echo '
+					            			<li>
+					                			<a href="#"><img id="profile_logo" src="img/profile.png"></a>
+					                				<ul class="sub-menu">
+					                					<form action="backend/validate.php" method="POST">
+					                    						<li><label for="username">Email</label>
+							<input placeholder="Enter Email" type="text" id="username" name="username" /></li>
+					                    						<li><label for="username">Password</label>
+							<input placeholder="Enter Password" type="password" id="password" name="password" /></li>
+					                    						<li><button type="submit" style="cursor:pointer;" class="btn btn-secondary">Submit</button></li>
+					                    				</form>
+					                				</ul>
+					            			</li>
+					            			';
+					        	?>
 					        </ul>
 					    </div>
 					</div>
