3175e416e34b7382c83c7dbd418d2336ee3ccca5
Glenn Perez <glennmatthew_perez@hotmail.com>
2018-03-11 21:19:54 +0800

    fix

--- js/funcs.js
+++ js/funcs.js
@@ -68,3 +68,19 @@ function payuscard(){
   xhttp.open("GET", "backend/pay-card.php?totalprice="+totalprice, true);
   xhttp.send();
 }
+
+function showSearchHint(str) {
+    if (str.length == 0) { 
+        document.getElementById("txtHint").innerHTML = "";
+        return;
+    } else {
+        var xmlhttp = new XMLHttpRequest();
+        xmlhttp.onreadystatechange = function() {
+            if (this.readyState == 4 && this.status == 200) {
+                document.getElementById("txtHint").innerHTML = this.responseText;
+            }
+        };
+        xmlhttp.open("GET", "/BakaAG_test2/php/backend_search.php?q=" + str, true);
+        xmlhttp.send();
+    }
+}
